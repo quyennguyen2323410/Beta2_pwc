3450d2609d796e8ea79826a40114411866765986
ducminh1804 <voducminh39@gmail.com>
2026-09-22 09:34:11 +0700

    feat: add service worker for asset precaching and Supabase API caching

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-290dd570'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mnhverdv6c4"
+    "revision": "0.rkak3570r"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
